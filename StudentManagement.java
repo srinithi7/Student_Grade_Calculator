d97ca1d13fbad3b68c2ddbd16fd59e8aa4806658
Srinithi7 <jsrinithi2005@gmail.com>
2026-07-07 12:02:11 +0530

    Added student input and total calculation

--- StudentManagement.java
+++ StudentManagement.java
@@ -20,8 +20,27 @@ public class StudentManagement {
             total += marks[i];
         }
 
+        double percentage = total / 5.0;
+
+        String grade;
+
+        if(percentage >= 90)
+            grade = "A+";
+        else if(percentage >= 80)
+            grade = "A";
+        else if(percentage >= 70)
+            grade = "B";
+        else if(percentage >= 60)
+            grade = "C";
+        else if(percentage >= 50)
+            grade = "D";
+        else
+            grade = "Fail";
+
         System.out.println("\nStudent Name : " + name);
         System.out.println("Total Marks : " + total);
+        System.out.println("Percentage : " + percentage);
+        System.out.println("Grade : " + grade);
 
         sc.close();
     }
