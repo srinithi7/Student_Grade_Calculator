0ea0dd45c791306e89ac946e2371b754f5b86bd5
25mx124 <25mx124@MCA.PSG>
2026-07-07 11:37:58 +0530

    Added student input and total calculation

--- StudentManagement.java
+++ StudentManagement.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class StudentManagement {
+
     public static void main(String[] args) {
-        System.out.println("Student Management System");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== Student Management System =====");
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        int[] marks = new int[5];
+        int total = 0;
+
+        for(int i = 0; i < 5; i++) {
+            System.out.print("Enter Subject " + (i+1) + " Marks: ");
+            marks[i] = sc.nextInt();
+            total += marks[i];
+        }
+
+        System.out.println("\nStudent Name : " + name);
+        System.out.println("Total Marks : " + total);
+
+        sc.close();
     }
 }
